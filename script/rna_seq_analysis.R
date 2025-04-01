6ad964d6601ba88e54bb29dd57a69fab50351e45
Harveen Nanda <harveenanda216@gmail.com>
2025-04-01 14:44:44 +0530

    Update rna_seq_analysis.R

--- script/rna_seq_analysis.R
+++ script/rna_seq_analysis.R
@@ -1,15 +1,15 @@
 library(DESeq2)
 
-# Load count data
+# Load count and metadata (example CSVs)
 counts <- read.csv("data/counts_matrix.csv", row.names = 1)
 colData <- read.csv("data/sample_metadata.csv", row.names = 1)
 
-# Create DESeq2 dataset
+# DESeq2 pipeline
 dds <- DESeqDataSetFromMatrix(countData = counts, colData = colData, design = ~ condition)
 dds <- DESeq(dds)
-
-# Results
 res <- results(dds)
+
+# Save result table
 write.csv(res, "results/deseq2_results.csv")
 
 # Volcano Plot
